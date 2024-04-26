b86764d20e83baed25ad8937045747cbc4865af5
SudoSantos <twentycodm@outlook.com>
2024-04-26 09:42:26 +0100

    Hero section added

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="discover">
+      <h1>Discover Elite Living: Real Estate Redefined</h1>
+      <p>
+        Explore our range of premium properties, offering a blend of modern
+        luxury and eco-conscious designs for living and business
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
