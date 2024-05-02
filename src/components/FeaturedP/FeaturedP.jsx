6cbe7fa526bb088e9d5b0256d361854bdaab0c45
SudoSantos <twentycodm@outlook.com>
2024-05-02 15:34:42 +0100

    FeaturedP modified

--- src/components/FeaturedP/FeaturedP.jsx
+++ src/components/FeaturedP/FeaturedP.jsx
@@ -1,86 +1,111 @@
-import React, { useState } from "react";
 import "./FeaturedP.css";
+
+import { React, useState } from 'react';
+
 import { FeaturedPropertiesdata } from "../../data/FeaturedPropertiesdata";
-import { FaChevronRight } from "react-icons/fa6";
-import { FaChevronLeft } from "react-icons/fa6";
+
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa6";
+
 import { TbCurrencyNaira } from "react-icons/tb";
+
 import { FaRegHeart } from "react-icons/fa6";
+
 import { BsArrowsAngleExpand } from "react-icons/bs";
+
 import { IoAddCircleOutline } from "react-icons/io5";
+
 import { LiaBedSolid } from "react-icons/lia";
 
 const FeaturedP = () => {
-  const [image, setImage] = useState(0);
-  const imageLength = FeaturedPropertiesdata.map(
-    (property, index) => property.image.length
-  );
+  const [imageIndex, setImageIndex] = useState({});
+
+  const handlePrevClick = (index) => {
+    setImageIndex((prevIndex) => ({
+      ...prevIndex,
+      [index]: prevIndex[index] - 1,
+    }));
+  };
+
+  const handleNextClick = (index) => {
+    setImageIndex((prevIndex) => ({
+      ...prevIndex,
+      [index]: prevIndex[index] + 1,
+    }));
+  };
 
   return (
     <section className="images-br">
       <div className="move">
         <button>Prev</button>
+
         <button>Next</button>
       </div>
+
       <div className="card-container">
-        {FeaturedPropertiesdata.map((property, index) => {
-          return (
-            <div className="col" key={index}>
-              <div className="status">
-                <a href="/">Featured</a>
-                <a href="/">For sale</a>
-              </div>
-              <FaChevronLeft
-                className="arr-left"
-                onClick={() => {
-                  image === 0
-                    ? setImage(imageLength - 1)
-                    : setImage((prev) => prev - 1);
-                }}
-              />
-              <img className='image' src={property.image[0]} alt="" />
-              <div className="plus">
-                <p>
-                  <TbCurrencyNaira className="naira" />
-                  {property.price}
-                </p>
-                <div>
-                  <button>
-                    <BsArrowsAngleExpand className="icon" />
-                  </button>
-                  <button>
-                    <FaRegHeart className="icon" />
-                  </button>
-                  <button>
-                    <IoAddCircleOutline className="icon" />
-                  </button>
-                </div>
-              </div>
-              <h4>{property.desc}</h4>
-              <p className="location">{property.location}</p>
-              <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-                <LiaBedSolid className="bedIcon" />
-                <p className="num">{property.nob}</p>
-              </div>
-              <p className="num">{property.type}</p>
-              <div className="short-line"></div>
-              <FaChevronRight
-                className="arr-right"
-                onClick={() => {
-                  image === imageLength - 1
-                    ? setImage(0)
-                    : setImage((prev) => prev + 1);
-                }}
-              />
+        {FeaturedPropertiesdata.map((property, index) => (
+          <div className="col" key={index}>
+            <div className="status">
+              <a href="/">Featured</a>
+
+              <a href="/">For sale</a>
             </div>
-            // <div className="card" key={index}>
-            //   <div>block</div>
-            // </div>
-          );
-        })}
+
+            <FaChevronLeft
+              className="arr-left"
+              onClick={() => handlePrevClick(index)}
+            />
+
+            <img
+              className="image"
+              src={property.image[imageIndex[index] || 0]}
+              alt=""
+            />
+
+            <FaChevronRight
+              className="arr-right"
+              onClick={() => handleNextClick(index)}
+            />
+
+            <div className="plus">
+              <p>
+                <TbCurrencyNaira className="naira" />
+
+                {property.price}
+              </p>
+
+              <div>
+                <button>
+                  <BsArrowsAngleExpand className="icon" />
+                </button>
+
+                <button>
+                  <FaRegHeart className="icon" />
+                </button>
+
+                <button>
+                  <IoAddCircleOutline className="icon" />
+                </button>
+              </div>
+            </div>
+
+            <h4>{property.desc}</h4>
+
+            <p className="location">{property.location}</p>
+
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <LiaBedSolid className="bedIcon" />
+
+              <p className="num">{property.nob}</p>
+            </div>
+
+            <p className="num">{property.type}</p>
+
+            <div className="short-line"></div>
+          </div>
+        ))}
       </div>
     </section>
   );
 };
 
-export 
-default FeaturedP;
+export default FeaturedP;
