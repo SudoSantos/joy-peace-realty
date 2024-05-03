bcea127d007f92c806ed82498429b71b6c0a68b7
sudosantos <twentycodm@outlook.com>
2024-05-03 16:45:57 +0100

    Finest section modified

--- src/components/Finest/Finest.jsx
+++ src/components/Finest/Finest.jsx
@@ -1,43 +1,73 @@
 import React from "react";
-import image from "../../assets/FeaturedPimages/house5.jpg";
+import { FinestProperties } from "../../data/FinestProperties";
+import { TbCurrencyNaira } from "react-icons/tb";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa6";
 import { LiaBedSolid } from "react-icons/lia";
 import "./Finest.css";
 
 const Finest = () => {
+
+  // function RandomIndex() {
+  //   let randomNumber = Math.floor(Math.random() * 5) + 1;
+  //   console.log(randomNumber);
+  // } 
   return (
-    <section className="container">
+    <section className="containr">
 
       <div className="header">
         <h1>Check Our Selection Of Finest Properties</h1>
       </div>
 
-      <div className="card-container">
-        <div className="card">
-          <img src={image} alt="" />
-          <div className="details">
-            <p className="desc">4 Bedroom Semi detatched duplex for sale</p>
-            <p className="price">100,000,000</p>
-          </div>
-        </div>
+      <div className="cd-container">
+        {FinestProperties.map((property, index) => {
+          return (
+            <div className="cd">
+              <p className="featured">Featured</p>
+              <div className="stat">
+                <button>For sale</button>
+                <button>Hot offer</button>
+              </div>
+              < FaChevronLeft className="ar-left" />
+              <img src={property.image[0]} alt="" />
+              <div className="details">
+                <p className="desc">{property.desc}</p>
+                <p className="price">< TbCurrencyNaira className="currency"/>{property.price}</p>
+                <div style={{ display: 'flex', gap: '5px' }}>
+                  < LiaBedSolid className="bed" />
+                  <span>{property.nob}</span>
+                </div>
+              </div>
+              < FaChevronRight className="ar-right" />
+            </div>
+          )
+        })}
 
-        <div className="card">
-          <img src={image} alt="" />
-          <div className="details">
-            <p className="desc">4 Bedroom Semi detatched duplex for sale</p>
-            <p className="price">100,000,000</p>
-          </div>
-        </div>
 
-        <div className="card">
+        {/* <div className="cd">
+          < FaChevronLeft className="ar-left" />
           <img src={image} alt="" />
           <div className="details">
             <p className="desc">4 Bedroom Semi detatched duplex for sale</p>
             <p className="price">100,000,000</p>
+            < LiaBedSolid className="bed" />
           </div>
-        </div>
+          < FaChevronRight className="ar-right" />
+        </div> */}
+
+        {/* <div className="cd">
+          < FaChevronLeft className="ar-left" />
+          <img src={image} alt="" />
+          <div className="details">
+            <p className="desc">4 Bedroom Semi detatched duplex for sale</p>
+            <p className="price">100,000,000</p>
+            < LiaBedSolid className="bed" />
+          </div>
+          < FaChevronRight className="ar-right" />
+        </div> */}
+
       </div>
 
-      
+
     </section>
   );
 };
---
+++ src/data/FinestProperties.jsx
@@ -0,0 +1,54 @@
+import pic1 from '../assets/FinestImages/1/101.jpg'
+import pic2 from '../assets/FinestImages/1/102.jpg'
+import pic3 from '../assets/FinestImages/1/103.jpg'
+import pic4 from '../assets/FinestImages/1/104.jpg'
+import pic5 from '../assets/FinestImages/1/105.jpg'
+import pic6 from '../assets/FinestImages/1/106.jpg'
+import pic7 from '../assets/FinestImages/1/201.jpg'
+import pic8 from '../assets/FinestImages/1/202.jpg'
+import pic9 from '../assets/FinestImages/1/203.jpg'
+import pic10 from '../assets/FinestImages/1/204.jpg'
+import pic11 from '../assets/FinestImages/1/205.jpg'
+import pic12 from '../assets/FinestImages/1/206.jpg'
+import pic13 from '../assets/FinestImages/1/207.jpg'
+import pic14 from '../assets/FinestImages/1/208.jpg'
+
+
+export  const FinestProperties = [
+    {
+        image:[pic1, pic2, pic3, pic14, pic13],
+        desc: 'Luxurious 5 Bedroom Full Detached',
+        price: '580,000,000',
+        nob: '5',
+    },
+    {
+        image:[pic4, pic5, pic6, pic12, pic11],
+        desc: ' 5 Bedroom Contemporary Full Detached',
+        price: '550,000,000',
+        nob: '5',
+    },
+    {
+        image:[pic7, pic8, pic9, pic10, pic3],
+        desc: '4 Bedroom Semi Detached',
+        price: '100,000,000',
+        nob: '4',
+    },
+    {
+        image:[pic10, pic11, pic12, pic1, pic2], 
+        desc: '4 Bedroom Water-View Detached',
+        price: '580,000,000',
+        nob: '4',
+    },
+    {
+        image:[pic13, pic14, pic1, pic5, pic7], 
+        desc: '2 Bedroom Serviced Apartment',
+        price: '60,000,000',
+        nob: '2',
+    },
+    {
+        image:[pic9, pic13, pic11, pic3, pic5], 
+        desc: '5 Bedroom Contemporary Full Detached',
+        price: '700,000,000',
+        nob: '5',
+    }
+];
