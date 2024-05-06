b6dc355afb7124ef05634d4fd2c28456c1542606
sudosantos <twentycodm@outlook.com>
2024-05-06 12:05:29 +0100

    Testimonials data file added

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import joy from '../../assets/joyJoseph.jpg';
-import peace from '../../assets/peaceJoseph.jpg';
+import { Agentsdata } from '../../data/Agentsdata';
 import './Agents.css';
 
 
 const Agents = () => {
     return (
         <section className="agentus">
-            
-      <h2 className="meet">Meet Our Agents</h2>
-            <div className="agents">
-                <div className="agent">
-                    <img src={joy} alt="" />
-                    <p className="name">Joy Joseph</p>
-                    <p className="role">CEO, JOY AND PEACE REALTY</p>
-                    <a href=" /">View Profile</a>
-                </div>
 
-                <div className="agent">
-                    <img src={peace} alt="" />
-                    <p className="name">Peace Joseph</p>
-                    <p className="role">CEO, JOY AND PEACE REALTY</p>
-                    <a href="/">View Profile</a>
-                </div>
+            <h2 className="meet">Meet Our Agents</h2>
+            <div className="agents">
+                {Agentsdata.map((agent, index) => {
+                    return (
+                        <div className="agent">
+                            <img src={agent.image} alt="" />
+                            <p className="name">{agent.name}</p>
+                            <p className="role">{agent.role}</p>
+                            <a href=" /">View Profile</a>
+                        </div>
+                    )
+                })}
+
 
             </div>
 
---
+++ src/data/Testimonialsdata.jsx
@@ -0,0 +1,8 @@
+import Matthew from '../assets/matthew.jpg'
+import Alex from '../../assets/alex.jpg'
+import Olawale from '../../assets/olawale.jpg'
+
+
+export const Testimonialsdata = [
+
+];
